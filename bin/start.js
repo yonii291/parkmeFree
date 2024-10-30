bceec3224f226736590fa203c901396f626689be
Yonatan Teklezgi <yonatan.teklezgi@heig-vd.ch>
2024-10-30 16:12:25 +0100

    first commit parkmeFree

---
+++ bin/start.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+import createDebugger from "debug";
+import http from "node:http";
+
+import app from "../app.js";
+
+const debug = createDebugger('parkme-free:server')
+
+// Get port from environment and store in Express
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+// Create HTTP server
+const httpServer = http.createServer(app);
+
+// Listen on provided port, on all network interfaces
+httpServer.listen(port);
+httpServer.on("error", onHttpServerError);
+httpServer.on("listening", onHttpServerListening);
+
+// Normalize a port into a number, string, or false
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+function onHttpServerError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+
+  // Handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+    default:
+      throw error;
+  }
+}
+
+function onHttpServerListening() {
+  const addr = httpServer.address();
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", function (req, res, next) {
+  res.send("Got a response from the users route");
+});
+
+export default router;

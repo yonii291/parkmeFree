51f8b325b5e34ccc4b13a6ec478087643e6e90b7
Lucie Demont <lucie.demont@heig-vd.ch>
2024-12-07 14:20:10 +0100

    Installation doc

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ import carsRouter from "./routes/cars.js";
 import usersRouter from "./routes/users.js";
 import parksRouter from "./routes/parks.js";
 import parkingSessionsRouter from "./routes/parkingSessions.js";
-import notificationsRouter from './routes/notifications.js';
-import mongoose from 'mongoose';
+import notificationsRouter from "./routes/notifications.js";
+import mongoose from "mongoose";
 
 ///DATABASE CONNECTION
-mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/monApp', {
-
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Error connecting to MongoDB', err));
+mongoose
+  .connect(process.env.DATABASE_URL || "mongodb://localhost/monApp", {})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB", err));
 
 const app = express();
+
+app.use("/documentation", express.static("documentation"));
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -28,11 +29,10 @@ app.use("/login", usersRouter);
 app.use("/parks", parksRouter);
 app.use("/cars", carsRouter);
 app.use("/parkingSession", parkingSessionsRouter);
-app.use('/notifications', notificationsRouter);
+app.use("/notifications", notificationsRouter);
 app.use("/", indexRouter);
 
 // mettre tous les routes objets
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/cars.js
+++ routes/cars.js
@@ -6,60 +6,76 @@
 
 // export default router;
 
-import express from 'express';
-import { Car } from '../model/Car.js';
-import authenticate from '../utils/auth.js';
+import express from "express";
+import { Car } from "../model/Car.js";
+import authenticate from "../utils/auth.js";
 const router = express.Router();
 
 // Get all cars
-router.get('/', function (req, res, next) {
-    Car.find().exec(function (err, cars) {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).send(cars);
-    });
+
+/**
+ * @api {get} /cars/:id Request a car's information
+ * @apiName GetCar
+ * @apiGroup Car
+ *
+ * @apiParam {Number} id Unique identifier of the car
+ *
+ * @apiSuccess {String} firstName First name of the user
+ * @apiSuccess {String} lastName  Last name of the user
+ *
+ * @apiError {String} message Error message
+ */
+router.get("/", function (req, res, next) {
+  Car.find().exec(function (err, cars) {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).send(cars);
+  });
 });
 
 // Get car by ID
-router.get('/:id', function (req, res, next) {
-    Car.findOne({ _id: req.params.id }).exec(function (err, car) {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).send(car);
-    });
+router.get("/:id", function (req, res, next) {
+  Car.findOne({ _id: req.params.id }).exec(function (err, car) {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).send(car);
+  });
 });
 
 // Create a new car
-router.post('/create', authenticate, function (req, res, next) {
-    const newCar = new Car(req.body);
-    newCar.save(function (err, savedCar) {
-        if (err) {
-            return next(err);
-        }
-        res.status(201).send(savedCar);
-    });
+router.post("/create", authenticate, function (req, res, next) {
+  const newCar = new Car(req.body);
+  newCar.save(function (err, savedCar) {
+    if (err) {
+      return next(err);
+    }
+    res.status(201).send(savedCar);
+  });
 });
 
 // Update a car by ID
-router.put('/:id', authenticate, function (req, res, next) {
-    Car.findByIdAndUpdate(req.params.id, req.body, { new: true }).exec(function (err, updatedCar) {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).send(updatedCar);
-    });
+router.put("/:id", authenticate, function (req, res, next) {
+  Car.findByIdAndUpdate(req.params.id, req.body, { new: true }).exec(function (
+    err,
+    updatedCar
+  ) {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).send(updatedCar);
+  });
 });
 
 // Delete a car by ID
-router.delete('/:id', authenticate, function (req, res, next) {
-    Car.findByIdAndRemove(req.params.id).exec(function (err, removedCar) {
-        if (err) {
-            return next(err);
-        }
-        res.status(200).send(removedCar);
-    });
+router.delete("/:id", authenticate, function (req, res, next) {
+  Car.findByIdAndRemove(req.params.id).exec(function (err, removedCar) {
+    if (err) {
+      return next(err);
+    }
+    res.status(200).send(removedCar);
+  });
 });
 
 export default router;

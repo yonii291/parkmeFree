ac327fa2d09c3f0199e95eb34d42981a985b150b
Hanae Ibrahim <hanae.ibrahimmohamed@heig-vd.ch>
2025-01-16 19:37:56 +0100

    corriger test delete

--- model/Car.js
+++ model/Car.js
@@ -27,16 +27,8 @@ const carSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
-
-<<<<<<< HEAD
 })
-=======
-  creationDate: {
-    type: Date,
-    default: Date.now,
-  },
-});
->>>>>>> 0b51acfb35ff5c82b40d359d8e341cf60b2c7ebf
+
 
 //Hide the _v to the api users
 carSchema.set("toJSON", {
